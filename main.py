b98c249216ace27630cd24b420cd4b122bd51327
AbdoulAlim idlbi <abdoulalim.idlbi@gmail.com>
2025-07-03 16:42:24 +0200

    update(ai model): changed AI model

--- main.py
+++ main.py
@@ -2,11 +2,20 @@ from diffusers import StableDiffusionPipeline
 import torch
 
 # Load model (first time download may be large ~4GB)
+
+# pipe = StableDiffusionPipeline.from_pretrained(
+#     "stabilityai/sdxl-turbo",
+#     torch_dtype=torch.float16,
+#     variant="fp16",
+#     # safety_checker=None  
+# )
+
 pipe = StableDiffusionPipeline.from_pretrained(
-    "stabilityai/sdxl-turbo",
+    "SG161222/Realistic_Vision_V5.1_noVAE",
     torch_dtype=torch.float16,
     variant="fp16",
 )
+
 pipe = pipe.to("cuda" if torch.cuda.is_available() else "cpu")
 
 prompt = input('What would you like to create? \n')
